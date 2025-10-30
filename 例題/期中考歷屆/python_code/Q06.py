0bc49753b50e9744d8980041848041eafb8537dd
Nighttone4478 <c0923132433@gmail.com>
2025-10-30 14:36:38 +0800

    更新 Q01 至 Q06 的範例輸出，新增分隔線以提升可讀性，並修正 Q06 的程式邏輯與輸出格式

--- 例題/期中考歷屆/python_code/Q06.py
+++ 例題/期中考歷屆/python_code/Q06.py
@@ -7,14 +7,14 @@ def main():
     n = int(input())
     point = [0] * (n + 1)
 
-    money = list(map(int,input().split()))
-    cards = [[0] for _ in range(n+1)]
+    bet = list(map(int,input().split()))
+    cards = [[] for _ in range(n+1)]
     first_card = input().split()
 
     first_card.append(first_card.pop(0))
     for i in range(len(first_card)):
         point[i] += card_to_point(first_card[i])
-        cards[i][0] = first_card[i]
+        cards[i].append(first_card[i])
     
     i = 0
     while i < n:
@@ -46,15 +46,13 @@ def main():
             
     for i in range(len(point)-1):
         if point[i] == 11:
-            print(f"Player{i} -{money[i]}")
+            print(f"Player{i+1} -{bet[i]}")
         elif point[i] == 10.5:
-            print(f"Player{i} +{money[i]}")
+            print(f"Player{i+1} +{bet[i]}")
         elif point[i] > point[-1]:
-            print(f"Player{i} +{money[i]}")
+            print(f"Player{i+1} +{bet[i]}")
         else:
-            print(f"Player{i} -{money[i]}")
-
-
+            print(f"Player{i+1} -{bet[i]}")
 
 if __name__ == "__main__":
     main()
